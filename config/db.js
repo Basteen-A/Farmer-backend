4b9bfab65be0fb01179f8e1a7fe65dc9326c38b1
Basteen-A <santhiyagubasteen@gmail.com>
2025-02-24 20:24:49 +0530

    yes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST || 'sql106.infinityfree.com',
-  user: process.env.DATABASE_USER || 'if0_37346247',
-  password: process.env.DATABASE_PASSWORD || 'Basteen1307',
-  database: process.env.DATABASE_NAME || 'if0_38381839_farmer',
+  host: process.env.DATABASE_HOST || 'sql12.freesqldatabase.com',
+  user: process.env.DATABASE_USER || 'sql12764417',
+  password: process.env.DATABASE_PASSWORD || 'S9QRgVZAhd',
+  database: process.env.DATABASE_NAME || 'sql12764417',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
